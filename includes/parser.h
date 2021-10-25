f762d7866aecdd660ba9de23653b67f9ea021118
nschumac <schumachernikolai@gmail.com>
2021-10-26 00:58:57 +0200

    parse file

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: nschumac <nschumac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 22:36:02 by jsiller           #+#    #+#             */
-/*   Updated: 2021/10/25 23:27:32 by nschumac         ###   ########.fr       */
+/*   Updated: 2021/10/26 00:57:17 by nschumac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,15 @@
 # include <minishell.h> 
 
 # define REDIRECTIONS "<>"
+# define ENDSTRING "\"\' "
+# define ENDCOMMAND "&|"
+
 t_cmds	*parse(char *str);
 
+int	parse_endstring(char **str, char **strbuf, t_cmds *cur);
+
+int	parse_redirections(char **str, char **strbuf, t_cmds *cur);
+
+int	parse_endcommands(char **str,char **strbuf, t_cmds *cur);
 
 #endif
