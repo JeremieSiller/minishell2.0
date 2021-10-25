f6fec70257fd7a9fa58e6a79c8c89c9ffd93c22f
nschumac <schumachernikolai@gmail.com>
2021-10-26 00:02:24 +0200

    added c files for parser functions, changed method to counting up the str pointer instead of indexing

--- sources/parser/parser.c
+++ sources/parser/parser.c
@@ -6,67 +6,65 @@
 /*   By: nschumac <nschumac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 22:38:44 by nschumac          #+#    #+#             */
-/*   Updated: 2021/10/25 23:55:17 by nschumac         ###   ########.fr       */
+/*   Updated: 2021/10/26 00:01:50 by nschumac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <parser.h>
 #include <utilities.h>
 
-int	parse_double(char *str, int *cnt, char **strbuf, t_cmds *cur)
+int	parse_double(char **str, char **strbuf, t_cmds *cur)
 {
 
 }
 
-int	parse_single(char *str, int *cnt, char **strbuf, t_cmds *cur)
+int	parse_single(char **str,char **strbuf, t_cmds *cur)
 {
 
 }
 
-int	parse_variable(char *str, int *cnt, char **strbuf, t_cmds *cur)
+int	parse_variable(char **str, char **strbuf, t_cmds *cur)
 {
 
 }
 
-int	parse_pipe(char *str, int *cnt, char **strbuf, t_cmds *cur)
-{
-	
-}
-
-int	parse_space(char *str, int *cnt, char **strbuf, t_cmds *cur)
+int	parse_pipe(char **str,char **strbuf, t_cmds *cur)
 {
 	
 }
 
-int parse_redirections(char *str, int *cnt, char **strbuf, t_cmds *cur)
+int	parse_space(char **str,char **strbuf, t_cmds *cur)
+{
+	
+}
+
+int parse_redirections(char **str, char **strbuf, t_cmds *cur)
 {
 
 }
 
 t_cmds	*parse(char *str)
 {	
-	int		cnt;
 	char	*strbuf;
 	t_cmds	*cur;
 
-	cnt = 0;
-	while (str[cnt])
+	while (str && *str)
 	{
-		if (str[cnt] == '\"' && parse_double(str, &cnt, &strbuf, cur))
+		if (*str == '\"' && parse_double(&str, &strbuf, cur))
 			return (clear_list(cur, NULL));
-		else if (str[cnt] == '\'' && parse_single(str, &cnt, &strbuf, cur))
+		else if (*str == '\'' && parse_single(&str, &strbuf, cur))
 			return (clear_list(cur, NULL));
-		else if (str[cnt] == '$' && parse_variable(str, &cnt, &strbuf, cur))
+		else if (*str == '$' && parse_variable(&str, &strbuf, cur))
 			return (clear_list(cur, NULL));
-		else if (str[cnt] == '|' &&	parse_pipe(str, &cnt, &strbuf, cur))	
+		else if (*str == '|' &&	parse_pipe(&str, &strbuf, cur))	
 			return (clear_list(cur, NULL));
-		else if (str[cnt] == ' ' && parse_space(str, &cnt, &strbuf, cur))
+		else if (*str == ' ' && parse_space(&str, &strbuf, cur))
 			return (clear_list(cur, NULL));
-		else if (ft_strchr(REDIRECTIONS, str[cnt]) && parse_redirections(str, &cnt, &strbuf, cur))
+		else if (ft_strchr(REDIRECTIONS, *str) && parse_redirections(&str, &strbuf, cur))
 			return (clear_list(cur, NULL));
-		else if (char_append(&strbuf, str[cnt]))
+		else if (char_append(&strbuf, *str))
 			return (clear_list(cur, NULL));
-		cnt++;
+		str++;
 	}
 	if (strbuf)
 		if(string_append(&cur->cmd, strbuf))
