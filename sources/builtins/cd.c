64123b378ad0cafd548232fe5c970dca20c19499
Jeremie Siller <jsiller@3-B-5.42heilbronn.de>
2021-10-28 16:54:57 +0200

    basic pwd and cd implementation

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -6,14 +6,28 @@
 /*   By: jsiller <jsiller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 21:20:46 by jsiller           #+#    #+#             */
-/*   Updated: 2021/10/28 16:36:54 by jsiller          ###   ########.fr       */
+/*   Updated: 2021/10/28 16:54:31 by jsiller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+// handle home
+// handle env -> set PWD ->set OLDPWD
 int	bt_cd(char **argv)
 {
+	argv++;
+	if (!*argv)
+	{
+		// handle home
+		// handle if home not set
+		return (0);
+	}
+	if (chdir(*argv) == -1)
+	{
+		ft_putstr_fd("minishell: cd: ", 2);
+		perror(*argv);
+		return (1);
+	}
 	return (0);
-	(void)argv;
 }
--- sources/builtins/pwd.c
+++ sources/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: jsiller <jsiller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 21:26:42 by jsiller           #+#    #+#             */
-/*   Updated: 2021/10/27 21:27:02 by jsiller          ###   ########.fr       */
+/*   Updated: 2021/10/28 16:47:58 by jsiller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 int	bt_pwd(char **argv)
 {
+	char	str[4096];
+
+	if (!getcwd(str, 4096))
+	{
+		perror("minishell");
+		return (1);
+	}
+	printf("%s\n", str);
 	return (0);
 	(void)argv;
 }
