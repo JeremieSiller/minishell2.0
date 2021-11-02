24049236eb24961818c4cf5bdf261068a9c9be61
Jeremie Siller <jsiller@3-B-7.42heilbronn.de>
2021-11-02 13:27:58 +0100

    [here_doc] added

--- sources/execute/redirect.c
+++ sources/execute/redirect.c
@@ -6,51 +6,63 @@
 /*   By: jsiller <jsiller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 21:25:38 by jsiller           #+#    #+#             */
-/*   Updated: 2021/11/02 12:52:16 by jsiller          ###   ########.fr       */
+/*   Updated: 2021/11/02 13:27:16 by jsiller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 #include <execute.h>
 
-static int	here_doc(t_cmds *data, t_execute *exec)
+static int	read_from_stdin(t_cmds *data, int fd[2], int i)
 {
-	int		i;
-	int		fd;
 	char	*str;
 
+	str = readline("> ");
+	while (str && ft_strncmp(data->in_dir[i]->path, str,
+			ft_strlen(data->in_dir[i]->path) + 1))
+	{
+		write(fd[1], str, ft_strlen(str));
+		write(fd[1], "\n", 1);
+		free(str);
+		str = readline("> ");
+	}
+	free(str);
+	if (!data->in_dir[i + 1] && dup2(fd[0], 0) == -1)
+	{
+		perror("minishell");
+		close(fd[0]);
+		close(fd[1]);
+		return (1);
+	}
+	return (0);
+}
+
+static int	here_doc(t_cmds *data)
+{
+	int		i;
+	int		fd[2];
+
 	i = 0;
-	while (data->in_dir[i])
+	while (data->in_dir && data->in_dir[i])
 	{
 		if (data->in_dir[i]->type == 1)
 		{
-			fd = open("___here_doc___", O_RDWR);
-			if (fd == -1)
+			if (pipe(fd) == -1)
 			{
 				perror("minishell: here_doc");
 				return (1);
 			}
-			str = readline("> ");
-			while (str && strncmp(data->in_dir[i]->path, str, ft_strlen(data->in_dir[i]->path) + 1))
-			{
-				write(fd, str, ft_strlen(str));
-				free(str);
-			}
-			free(str);
-			if (dup2(fd, 0) == -1)
-			{
-				perror("minishell");
-				close(fd);
+			if (read_from_stdin(data, fd, i) == 1)
 				return (1);
-			}
-			close(fd);
+			close(fd[0]);
+			close(fd[1]);
 		}
 		i++;
 	}
 	return (0);
 }
 
-static int	out_directions(t_cmds *data, t_execute *exec)
+static int	out_directions(t_cmds *data)
 {
 	int	i;
 	int	fd;
@@ -61,14 +73,13 @@ static int	out_directions(t_cmds *data, t_execute *exec)
 		if (data->out_dir[i]->type == 0)
 			fd = open(data->out_dir[i]->path, O_RDWR | O_CREAT | O_TRUNC, 0644);
 		else
-			fd = open(data->out_dir[i]->path, O_RDWR | O_CREAT | O_APPEND, 0644);
+			fd = open(data->out_dir[i]->path, O_RDWR
+					| O_CREAT | O_APPEND, 0644);
 		if (fd == -1)
-		{
 			perror(data->out_dir[i]->path);
-			exec->exit = 1;
+		if (fd == -1)
 			return (1);
-		}
-		if (dup2(fd, 1) == -1);
+		if (dup2(fd, 1) == -1)
 		{
 			perror("minishell");
 			close(fd);
@@ -80,7 +91,7 @@ static int	out_directions(t_cmds *data, t_execute *exec)
 	return (0);
 }
 
-static int	in_directions(t_cmds *data, t_execute *exec)
+static int	in_directions(t_cmds *data)
 {
 	int	i;
 	int	fd;
@@ -94,7 +105,6 @@ static int	in_directions(t_cmds *data, t_execute *exec)
 			if (fd == -1)
 			{
 				perror(data->in_dir[i]->path);
-				exec->exit = 1;
 				return (1);
 			}
 			if (dup2(fd, 0) == -1)
@@ -106,11 +116,25 @@ static int	in_directions(t_cmds *data, t_execute *exec)
 		}
 		i++;
 	}
+	return (0);
 }
 
 int	redirect(t_cmds *data, t_execute *exec)
 {
-	if (in_directions(data, exec) == 1)
+	if (here_doc(data) == 1)
+	{
+		exec->exit = 1;
 		return (1);
-	return (out_directions(data, exec));
+	}
+	if (in_directions(data) == 1)
+	{
+		exec->exit = 1;
+		return (1);
+	}
+	if (out_directions(data) == 1)
+	{
+		exec->exit = 1;
+		return (1);
+	}
+	return (0);
 }
